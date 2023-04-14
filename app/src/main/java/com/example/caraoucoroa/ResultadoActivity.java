93fbe29019f01cd5f6cf5d6ba1f0bc71bfbe85dd
gabriel <gaamdal557@gmail.com>
2023-04-13 23:02:01 -0300

    interface finalizada

--- app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
@@ -7,21 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultadoActivity extends AppCompatActivity {
 
-    Button btnJogar;
+    Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_resultado);
 
-        btnJogar = findViewById(R.id.btnJogar);
+        btnVoltar = findViewById(R.id.btnVoltar);
 
-        btnJogar.setOnClickListener(new View.OnClickListener() {
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
